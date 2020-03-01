b634859390e1a20bc64a4b36ac98d95fe610c196
Kishor R <Kishor R@DESKTOP-OACM80T>
2020-03-01 16:31:34 +0500

    Fist Commit

---
+++ src/main/java/com/app/GitOperation2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitOperation2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitOperation2Application.class, args);
+	}
+
+}
