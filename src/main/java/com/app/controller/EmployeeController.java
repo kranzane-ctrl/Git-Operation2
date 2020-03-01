03ae3f1f6f24c3631c4b3419df72622f58a940c3
Kishor R <Kishor R@DESKTOP-OACM80T>
2020-03-02 04:31:55 +0500

    logging messages add

--- src/main/java/com/app/controller/EmployeeController.java
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -1,5 +1,9 @@
 package com.app.controller;
 
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +24,21 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 	
+	private static Logger log=Logger.getLogger(EmployeeController.class);
+	
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public ModelAndView loadWelcomePage() {
+		PropertyConfigurator.configure(System.getProperty("user.dir")+ File.separator + "log4j.properties");
+		log.info("Welcome Page to Load");
 		ModelAndView mav=new ModelAndView("welcome");
 		return mav;
 	}
 	
 	@RequestMapping(value = "/registration" , method = RequestMethod.GET)
 	public ModelAndView loadLoginPage() {
+		PropertyConfigurator.configure(System.getProperty("user.dir")+ File.separator + "log4j.properties");
+		log.info("Open to Registration Page");
+		
 		ModelAndView mav=new ModelAndView("login");
 		mav.addObject("welMsg", "Welcome to Registration Page");
 		mav.addObject("emp", new Employee());
@@ -36,8 +47,14 @@ public class EmployeeController {
 	
 	@RequestMapping(value = "/review", method = RequestMethod.POST)
 	public ModelAndView reviewData(@ModelAttribute("emp") Employee emp) {
+		PropertyConfigurator.configure(System.getProperty("user.dir")+ File.separator + "log4j.properties");
+		log.info("Data bind by @ModelAttributive to save");
+		
 		ModelAndView mav=new ModelAndView("review");
+		log.info("data saved to DB");
 		EmployeeEntity saveEntity = service.saveEntity(emp);
+		
+		log.info("Data Successfully Saved to Database");
 		mav.addObject("reviewData", "Please Review Your Data And Confirm ");
 		mav.addObject("empReview", saveEntity);
 		return mav;
@@ -45,6 +62,9 @@ public class EmployeeController {
 	
 	@RequestMapping(value = "/findById", method = RequestMethod.GET)
 	public ModelAndView findById() {
+		PropertyConfigurator.configure(System.getProperty("user.dir")+ File.separator + "log4j.properties");
+		log.info("findDataById to Load");
+		
 		ModelAndView mav=new ModelAndView("findDataById");
 		return mav;
 	}
